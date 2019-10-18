5d337c93eeae8b7ac7ec9974925be3ee3f9d0d0e
Moustanir <moustanir.soule-ali@ynov.com>
2019-10-18 14:43:34 +0200

    Front ok

--- view/script.js
+++ view/script.js
@@ -1,5 +1,6 @@
 $(document).ready(()=>{
-    let server = "silex"
+    let server = "silex";
+    //Client
     let optionsAjaxGetClients = {
         "url":`http://${server}/client`,
         "type":"GET"
@@ -7,24 +8,10 @@ $(document).ready(()=>{
     function formatData(value){
         return (value == "null" || value == null || value == "") ? "non précisé" : value;
     }
-    function getClientOrders(id){
-        let optionsAjaxGetClients = {
-            "url":`http://${server}/client/${id}/commandes`,
-            "type":"GET"
-        }; 
-        $.ajax(optionsAjaxGetClients).then(response=>{
-            if(response.success){
-                let orders = response.results.users,
-                    tbodyOrders = document.getElementById("tbody-orders");
-                orders.forEach(element => {
-                    prepareRowOrder(element,tbodyOrders);
-                });
-            }
-        });
-    }
     function prepareRowClient(client,tbody){
         let row = `
-                <tr id="${formatData(client.id_client)}">
+                <tr>
+                    <td>${formatData(client.id_client)}</td>
                     <td>${formatData(client.nom)}</td>
                     <td>${formatData(client.prenom)}</td>
                     <td>${formatData(client.adresse)}</td>
@@ -33,22 +20,86 @@ $(document).ready(()=>{
                     <td>${formatData(client.numero)}</td>
                     <td>${formatData(client.ville)}</td> 
                     <td>
-                    <button class="btn btn-info" id="order--{${formatData(client.id_client)}" data-client="${formatData(client.id_client)}" >Afficher les commandes</button>
+                    <button class="btn btn-info" id="clientOrder--${client.id_client}">Afficher les commandes</button>
                     </td>
                 </tr>
         `;
+
         $(tbody).append(row);
-        $(`order--${formatData(client.id_client)}`).click(()=>{
-            console.log("J'affiche les commandes");
-        })
+        $(`#clientOrder--${client.id_client}`).click(()=>{
+            getClientOrders(client.id_client);
+        });
     }
+    
     $.ajax(optionsAjaxGetClients).then(response=>{
         if(response.success){
-            let clients = response.results.clients,
+            let clients = response.results.data,
                 tbodyClients = document.getElementById("tbody-client");
             clients.forEach(element => {
                 prepareRowClient(element,tbodyClients);
             });
         }
     });
+    //Order
+    function getClientOrders(id){
+        let optionsAjaxGetOrders = {
+            "url":`http://${server}/client/${id}/commande`,
+            "type":"GET"
+        }; 
+        $.ajax(optionsAjaxGetOrders).then(response=>{
+            if(response.success){
+                let orders = response.results.data,
+                    tbodyOrders = document.getElementById("tbody-orders");
+                orders.forEach(element => {
+                    prepareRowOrder(element,tbodyOrders);
+                });
+            }
+        });
+    }
+    function prepareRowOrder(order,tbody){
+        tbody.innerHTML = ""
+        let row = `
+                <tr>
+                    <td>${formatData(order.numero)}</td>
+                    <td>${formatData(order.date_commande)}</td>
+                    <td>
+                        <button class="btn btn-info" id="orderProduits--${order.id_commande}" >Afficher les produits associés</button>
+                    </td>
+                </tr>
+        `;
+        $(tbody).append(row);
+        $(`#orderProduits--${order.id_commande}`).click(()=>{
+            getOrderProducts(order.id_commande);
+        });
+    }
+    //Produits
+    function getOrderProducts(id){
+        let optionsAjaxGetOrders = {
+            "url":`http://${server}/commande/${id}/produit`,
+            "type":"GET"
+        }; 
+        $.ajax(optionsAjaxGetOrders).then(response=>{
+            if(response.success){
+                let produits = response.results.data,
+                    tbodyOrders = document.getElementById("tbody-produits");
+                console.log(`Length:${produits.length}`);
+                produits.forEach(element => {
+                    prepareRowProduct(element,tbodyOrders);
+                });
+            }
+        });
+    }
+    function prepareRowProduct(product,tbody){
+        //tbody.innerHTML = ""
+        let row = `
+                <tr>
+                    <td>${formatData(product.libelle)}</td>
+                    <td>${formatData(product.prix_unitaire)}</td>
+                    <td>${formatData(product.reference)}</td>
+                    <td>${formatData(product.fournisseur)}</td>
+                    <td>${formatData(product.ville)}</td>
+                </tr>
+        `;
+        $(tbody).append(row);
+    }
 });
